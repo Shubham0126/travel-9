b6e7a99c15a14bb3289f019b415b940c9f285850
Shubham Pal <115461478+Shubham0126@users.noreply.github.com>
2023-05-23 19:01:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+const modal = document.querySelector(".modall");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".modall .close-btn");
+const discountBtn = document.querySelector(".modall .discount-btn");
+
+const createCookie = () => {
+  let maxAge = ";max-age=10";
+  let path = ";path=/";
+  document.cookie = "live-blogger-popup=displayed" + maxAge + path;
+};
+
+const displayModal = () => {
+  if (document.cookie.indexOf("live-blogger-popup") == -1) {
+    modal.classList.add("active");
+    modalOverlay.classList.add("active");
+    createCookie();
+  }
+};
+
+setTimeout(() => {
+  displayModal();
+}, 2000);
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+  modalOverlay.classList.remove("active");
+});
+
+discountBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+  modalOverlay.classList.remove("active");
+});
+
+$(document).ready(function(){
+    $('.con-btn').click(function(){ 
+      var popupBlock = $('#'+$(this).data('popup'));
+      popupBlock.addClass('active')
+        .find('.fade-out').click(function(){
+          popupBlock.css('opacity','0').find('.popup-content').css('margin-top','350px');        
+          setTimeout(function(){
+            $('.popup').removeClass('active');
+            popupBlock.css('opacity','').find('.popup-content').css('margin-top','');
+          }, 600);
+        });
+   });
+  });
+
+
 $(".place-carousel").owlCarousel({
     autoplay: true,
     smartSpeed: 1500,
